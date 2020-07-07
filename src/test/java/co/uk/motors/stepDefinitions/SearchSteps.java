b191444a91ad4f12cf59e88be42e63f6517ba212
tunde2018 <tunadng@yahoo.com>
2020-07-07 09:18:00 +0100

    commiting initial framework setup

---
+++ src/test/java/co/uk/motors/stepDefinitions/SearchSteps.java
@@ -0,0 +1,57 @@
+package co.uk.motors.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SearchSteps
+{
+    @Given("I navigate to Motors homepage")
+    public void i_navigate_to_Motors_homepage() {
+
+    }
+
+    @When("I enter {string} into the search field")
+    public void i_enter_into_the_search_field(String string) {
+
+    }
+
+    @When("I select {string} from any car make")
+    public void i_select_from_any_car_make(String string) {
+
+    }
+
+    @When("I select {string} from any car model")
+    public void i_select_from_any_car_model(String string) {
+
+    }
+
+    @When("I select {string} from Min price")
+    public void i_select_from_Min_price(String string) {
+
+    }
+
+    @When("I select {string} from Max price")
+    public void i_select_from_Max_price(String string) {
+
+    }
+
+    @When("I click on Search button")
+    public void i_click_on_Search_button() {
+
+    }
+
+    @Then("the search {string} is displayed")
+    public void the_search_is_displayed(String string) {
+
+    }
+    @Then("an error page is displayed")
+    public void an_error_page_is_displayed() {
+
+    }
+
+    @When("I select <{string} from any car make")
+    public void iSelectFromAnyCarMake(String arg0) {
+    }
+}
+
